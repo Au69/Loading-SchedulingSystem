21f5dc7ebfdb82ee6b54d9995386cc77cf74244d
Zaen <119846497+Au69@users.noreply.github.com>
2024-10-26 06:30:47 -0700

    Delete SchedulingSystem directory

--- SchedulingSystem/includes/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Check if the user is logged in
-if (!isset($_SESSION['username'])) {
-    header("Location: login.php"); // Redirect to login page if not logged in
-    exit();
-}
-?>
